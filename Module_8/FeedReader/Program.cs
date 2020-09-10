e204fcd9ac2b110733fe25b8187579bafec61cbe
Patrick Schmidt <pschmidt@pasit.nl>
2020-09-10 14:30:01 +0200

    Reader

---
+++ Module_8/FeedReader/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace FeedReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UsingWebRequest();
+            UsingHttpClient();
+            Console.ReadLine();  
+        }
+
+        private static async Task UsingHttpClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://nu.nl/");
+            var response = await client.GetAsync("rss");
+            var str = await response.Content.ReadAsStreamAsync();
+            //HandleWithLinqToXml(str);
+            HandleWithXmlSerializer(str);
+        }
+
+        private static void UsingWebRequest()
+        {
+            HttpWebRequest req = WebRequest.Create("https://nu.nl/rss") as HttpWebRequest;
+            HttpWebResponse response =  req.GetResponse() as HttpWebResponse;
+            //HandleWithXmlSerializer(response.GetResponseStream());
+            HandleWithLinqToXml(response.GetResponseStream());
+        }
+
+        private static void HandleWithLinqToXml(Stream stream)
+        {
+            XDocument doc = XDocument.Load(stream);
+
+            var query = from item in doc.Descendants("item")
+            select new Item
+            {
+                Category = item.Element("category").Value,
+                Title = item.Element("title").Value,
+                Description = item.Element("description").Value
+            };
+
+            ShowItems(query.ToList());
+
+        }
+        private static void HandleWithXmlSerializer(Stream stream)
+        {
+            List<Item> news = new List<Item>();
+
+            XmlSerializer ser = new XmlSerializer(typeof(Item));
+
+            XmlReader rdr = XmlReader.Create(stream);
+            while (rdr.ReadToFollowing("item"))
+            {
+                XmlReader sub = rdr.ReadSubtree();
+                Item ietm = ser.Deserialize(sub) as Item;
+                news.Add(ietm);
+            }
+            ShowItems(news);
+        }
+
+        private static void ShowItems(List<Item> news)
+        {
+            foreach(Item item in news)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(item.Category);
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(item.Title);
+                Console.ResetColor();
+                Console.WriteLine(item.Description);
+                Console.WriteLine("============================================================");
+            }
+        }
+    }
+}
---
+++ Module_8/Remoting/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Remoting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DemoWebRequests();
+            DemoHttpClient();
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        private static async Task DemoHttpClient()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+
+            //handler.Credentials = CredentialCache.DefaultCredentials;
+            handler.Credentials = new NetworkCredential("", "");        
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.xs4all.nl/");
+            
+            var result = await client.GetAsync("");
+            Console.WriteLine(result.StatusCode);
+            string body = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+
+        private static void DemoWebRequests()
+        {
+            HttpWebRequest req = WebRequest.Create("https://www.xs4all.nl") as HttpWebRequest;
+            req.Headers.Add("Accept", "text/html");
+            req.Method = "GET";
+            //req.Credentials = new NetworkCredential("user", "pass");
+
+            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+            Console.WriteLine(resp.StatusCode);
+            Console.WriteLine(resp.Headers["content-type"]);
+
+            Stream str = resp.GetResponseStream();
+            StreamReader rdr = new StreamReader(str);
+            string result = rdr.ReadToEnd();
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
