e30a54f2fcac57fe375b570c6a456970ee3bef3a
Patrick Schmidt <pschmidt@pasit.nl>
2020-09-11 11:38:26 +0200

    Crypto

---
+++ Module_13/Confidentiality/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Confidentiality
+{
+    class Program
+    {
+        private static byte[] sharedKey;
+        private static byte[] iv;
+
+        static void Main(string[] args)
+        {
+            Symmetric();
+            //Asymmetric();
+        }
+
+        private static void Symmetric()
+        {
+            AesManaged alg = new AesManaged();
+            sharedKey = alg.Key;
+            iv = alg.IV;
+            
+            // Sender
+            string document = "Hello World";
+            byte[] cipher;
+            using (MemoryStream mem = new MemoryStream())
+            {
+                using (CryptoStream cryp = new CryptoStream(mem, 
+                                                                        alg.CreateEncryptor(), 
+                                                                        CryptoStreamMode.Write))
+                {
+                    using (StreamWriter writer = new StreamWriter(cryp))
+                    {
+                        writer.WriteLine(document);
+                    }
+                }
+                cipher = mem.ToArray();
+            }
+            Console.WriteLine(Encoding.UTF8.GetString(cipher));
+
+
+            // Recepient
+            AesManaged rAlg = new AesManaged();
+            rAlg.Key = sharedKey;
+            rAlg.IV = iv;
+
+            using(MemoryStream mem = new MemoryStream(cipher))
+            {
+                using(CryptoStream cryp = new CryptoStream(mem, 
+                                                                           rAlg.CreateDecryptor(), 
+                                                                           CryptoStreamMode.Read))
+                {
+                    using (StreamReader rdr = new StreamReader(cryp))
+                    {
+                        string data = rdr.ReadLine();
+                        Console.WriteLine(data);
+                    }
+                }
+            }
+        }
+
+        private static void Asymmetric()
+        {
+            // Recepient buys a pub & private key pair
+            RSACryptoServiceProvider alg = new RSACryptoServiceProvider();
+            string pubKey = alg.ToXmlString(false);
+            string privKey = alg.ToXmlString(true);
+
+
+            // Sender receives the public key and creates a document (or whatever)
+            string document = "Hello World";
+            RSACryptoServiceProvider sAlg = new RSACryptoServiceProvider();
+            sAlg.FromXmlString(pubKey);
+            byte[] data = Encoding.UTF8.GetBytes(document);
+            byte[] cipher = sAlg.Encrypt(data, true);
+
+            Console.WriteLine(Encoding.UTF8.GetString(cipher));
+
+            // Send cipher to recepient
+            RSACryptoServiceProvider rAlg = new RSACryptoServiceProvider();
+            rAlg.FromXmlString(privKey);
+            byte[] orgiData = rAlg.Decrypt(cipher, true);
+            Console.WriteLine(Encoding.UTF8.GetString(orgiData));
+
+
+        }
+    }
+}
---
+++ Module_13/Integrity/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Integrity
+{
+    class Program
+    {
+        // Make sure only sender and recepient knows this secret.
+        private static byte[] salt;
+
+        static void Main(string[] args)
+        {
+            //Symmetric();
+            Asymmetric();
+        }
+
+        private static void Asymmetric()
+        {
+            // Sender
+            string document = "Hello World";
+            DSACryptoServiceProvider alg = new DSACryptoServiceProvider();
+            string publicKey = alg.ToXmlString(false);
+            Console.WriteLine(publicKey);
+            byte[] hash = CreateHash(document);
+            byte[] digitalSignature = alg.SignHash(hash, "SHA1");
+
+            // ED at work
+            document += ".";
+
+            // Send it to recepient
+            bool isOK = VerifySignature(document, digitalSignature, publicKey);
+            Console.WriteLine(isOK ? "OK" : "NOT OK");
+        }
+
+        private static bool VerifySignature(string document, byte[] digitalSignature, string publicKey)
+        {
+            DSACryptoServiceProvider alg = new DSACryptoServiceProvider();
+            alg.FromXmlString(publicKey);
+            byte[] hash = CreateHash(document);
+            bool isOK = alg.VerifyHash(hash, "SHA1", digitalSignature);
+            return isOK;
+        }
+
+        private static void Symmetric()
+        {
+            // Sender
+            string document = "Hello World";
+            byte[] hash = CreateHash(document);
+
+            // ED tries to hack
+            document += "!";
+            //HMACSHA1 alg = new HMACSHA1();
+            //byte[] edbuffer = Encoding.UTF8.GetBytes(document);
+            //byte[] edhash = alg.ComputeHash(edbuffer);
+
+            // Recepient
+            bool isOk = CheckDocument(document, hash);
+            Console.WriteLine(isOk ? "OK" : "NOT OK");
+        }
+
+        private static bool CheckDocument(string document, byte[] hash)
+        {
+            //SHA1Managed alg = new SHA1Managed();
+            HMACSHA1 alg = new HMACSHA1();
+            alg.Key = salt;
+            byte[] buffer = Encoding.UTF8.GetBytes(document);
+            byte[] hash2 = alg.ComputeHash(buffer);
+            for(int i = 0; i < hash.Length; i++)
+            {
+                if (hash[i] != hash2[i]) return false;
+            }
+
+            return true;
+        }
+
+        private static byte[] CreateHash(string document)
+        {
+            // Uses underlying c-library
+            //SHA1CryptoServiceProvider alg = new SHA1CryptoServiceProvider();
+            // Rewritten in managed code (.net)
+            SHA1Managed alg = new SHA1Managed();
+            //HMACSHA1 alg = new HMACSHA1();
+            //salt = alg.Key;
+            byte[] buffer = Encoding.UTF8.GetBytes(document);
+            byte[] hash = alg.ComputeHash(buffer);
+            
+            Console.WriteLine(Convert.ToBase64String(hash));
+            
+            return hash;
+        }
+    }
+}
