6d17251d7d1768aabecfbcf08800a3b05b285996
Patrick Schmidt <pschmidt@pasit.nl>
2020-09-11 09:33:07 +0200

    ok

--- Module_11/GarbageMan/Program.cs
+++ Module_11/GarbageMan/Program.cs
@@ -4,23 +4,23 @@ namespace GarbageMan
 {
     class Program
     {
+        static UMan m1 = new UMan();
         static void Main(string[] args)
-        {
-            UMan m1 = new UMan();
+        {           
             try
             {
                 m1.Open();
             }
             finally
             {
-                m1.Dispose();
+                //m1.Dispose();
             }
             m1 = null;
 
-            //GC.Collect();
-            //GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
 
-            using (UMan m2 = new UMan())
+            using(UMan m2 = new UMan())
             {
                 m2.Open();
             }
